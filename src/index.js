ba0d46bb1a74bf4f43516dc44e094633f4a4d076
dmtaddict <dmtaddict@gmail.com>
2019-10-03 09:14:25 +0700

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let template = bracketsConfig.map(function(bracket){
+      return bracket;
+  });
+  let brckt=[];
+    for(let i = 0; i < str.length; i++){
+        template.map(function (bracket) {
+            if(str[i] === bracket[1]&&str[i] === bracket[0]&&brckt.length === 0){
+                brckt.push(str[i]);
+            }
+            else if(str[i] === bracket[1]&&brckt.length === 0){
+                brckt.push('i dont know how to stop it'); ;
+            }
+            else if(str[i] === bracket[1]&&str[i] === bracket[0]&&str[i] !== brckt[brckt.length-1]){
+                brckt.push(str[i]);
+            }
+            else if(str[i] === bracket[1]&& bracket[0] === brckt[brckt.length-1]){
+                brckt.pop();
+            }
+            else if(str[i] === bracket[0]){
+                brckt.push(str[i]);
+            }
+
+        });
+    }
+    return brckt.length === 0;
+    // your solution
 }
